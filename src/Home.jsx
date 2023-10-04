599fbeb2c12aa7e207233a9eec12ef2f5cbf2491
jyot4 <jyotivyass2000@gmail.com>
2023-10-04 14:31:27 +0530

    my commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
+
+const [listofProduct ,setListofProduct] = useState([])
+
+
+  useEffect(()=>{
+    console.log("priya")
+    axios.get("http://localhost:8080/products")
+    .then((result)=>{
+// console.log(result.data)
+
+setListofProduct(result.data)
+
+    })
+  },[])
+// console.log(`"http://localhost:8080"+${listofProduct[0].photo.path}`);
+
+//   useEffect(()=>{
+//     axios.get('http://localhost:8080/uploads/products')
+//     .then((result)=>{
+// console.log(result)
+//     })
+//   },[])
   return (
-    <div>Home</div>
+
+
+    <div className='Home'>
+      <ul>
+        <li><Link to= ''>Clothing</Link></li>
+        <li><Link to= ''>Books</Link></li>
+        <li><Link to= ''>Electoroines</Link></li>
+        <li><Link to= ''>Beauty And Personalize</Link></li>
+      </ul>
+
+{
+  (listofProduct)?
+  listofProduct.map((lists, index)=>{
+    return(
+      <div>
+      <h1 key={index}>{lists.name}</h1>
+     <img src={"http://localhost:8080/" + lists.photo.path.split("//")[0] + "/" + lists.photo.path.split("//")[1]} alt='avaliable'/>
+        </div>
+    )
+ 
+  })
+  :" "
+}
+
+
+</div>
+
+
   )
 }
 
